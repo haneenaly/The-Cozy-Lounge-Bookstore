9c77db194d76b4c6b5708a167f6dc417f8697443
Haneen Aly Abdelkader <139086138+haneenaly@users.noreply.github.com>
2024-12-21 20:46:28 +0200

    Delete  directory

--- onlinebookstoreElTop/onlinebookstore/TheCozyLounge/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThecozyloungeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TheCozyLounge'
-
-    # def ready(self):
-    #     import TheCozyLounge.signals
