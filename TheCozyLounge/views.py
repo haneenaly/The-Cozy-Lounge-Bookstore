93b106a5a4f3f5baea17c5f5b78d3d34154f5180
Habiba Yahya <H.Yahya2214@nu.edu.eg>
2024-12-27 20:29:11 +0200

    Fixes issue #19 -Develop checkout page

--- TheCozyLounge/views.py
+++ TheCozyLounge/views.py
@@ -50,3 +50,45 @@ def loginPage(request):
 				messages.info(request,'Username OR Password is incorrect')
 		context={}
 		return render(request, 'accounts/login.html',context)   
+	
+@login_required(login_url='login')
+def checkout(request):
+	print(request)
+	data = cartData(request)
+	
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/checkout.html', context)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	else:
+		customer, order = guestOrder(request, data)
+
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	#check the total is correct or not 
+	if total == float(order.get_cart_total):
+		order.complete = True
+	order.save()
+
+	if order.shipping == True:
+		ShippingAddress.objects.create(
+		customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],
+		)
+
+	return JsonResponse('Payment submitted..', safe=False)
